e126473f2cf08c0f76cee173a6da407f5a45718f
Ajiroghene Jonathan Omudhewaefe <ajirijoe@yahoo.com>
2022-09-12 20:20:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	char c;
-	char c
+	char c;
 	int i;
 	long li;
 	long long lli;
@@ -18,8 +18,9 @@ int main(void)
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li))
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long 
+int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
