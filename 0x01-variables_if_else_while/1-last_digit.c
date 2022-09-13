5f2fae94ddd5ca46c3a243fca21115f2659dd7fd
Ajiroghene Jonathan Omudhewaefe <ajirijoe@yahoo.com>
2022-09-13 08:41:07 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int l;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-l = N % 10;
+l = n % 10;
 
 if (1 > 5)
 {
@@ -28,5 +28,6 @@ else if (1 ==0)
 else
 {
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, 1);
+}
 return (0);
 }
