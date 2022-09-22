8f82493e66a3925ec95f8fecfb11aad1ffe541e2
Ajiroghene Jonathan O.Efe <ajirijoe@yahoo.com>
2022-09-22 16:25:02 -0700

    worked on the prototype fnc for 0
    -strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the
- *	terminating null byte, to the end of the string pointed to by @dest.
+ * _strcat - concatenates  the string pointed to by @src to
+ *	the end of the string pointed to by @dest
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
  *
- * Return: A pointer to the destination string@dest.
+ * Return: returns poiner to @dest
  */
 
 char *_strcat(char *dest, char *src)
