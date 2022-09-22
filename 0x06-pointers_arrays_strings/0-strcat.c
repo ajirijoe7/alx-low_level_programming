6c15f053814187bf72cc1cad187f6e892ba5763a
Ajiroghene Jonathan O.Efe <ajirijoe@yahoo.com>
2022-09-22 15:38:51 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,20 @@
 
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int m = 0;
+	int n = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[m] != '\0')
+		m++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (src[n] != '\0')
+	{
+		dest[m] = src[n];
+		m++;
+		n++;
+
+	}
+	dest[m] = '\0';
 
 	return (dest);
 }
