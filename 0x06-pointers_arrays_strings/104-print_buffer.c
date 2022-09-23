15bc391a0ebbe0d1af7ebcb1dbb2d14fc68d9945
Ajiroghene Jonathan O.Efe <ajirijoe@yahoo.com>
2022-09-22 17:33:08 -0700

    104-print_buffer.c

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+
