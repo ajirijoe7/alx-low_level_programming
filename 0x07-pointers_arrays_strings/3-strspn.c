aeda012e925344269b23cf16b4a3225c6f681118
Ajiroghene Jonathan O.Efe <ajirijoe@yahoo.com>
2022-09-26 16:58:16 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,21 +12,20 @@ unsigned int _strspn(char *s, char *accept)
 	int i = 0, j;
 	int matches = 0;
 
-	while (s[i] != '\0') /*iterate through string*/
+	while (*(s + i))
 	{
 
-		for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
+		for (j = 0; *(accept + j); j++)
 		{
-			if (s[i] == accept[j]) /*record & break at first match*/
+			if (*(s + i) == *(accept + j))
 			{
 				matches++;
 				break;
 			}
-			if (accept[j + 1] == '\0' && s[i] != accept[j])
+			if (*(accept + j + 1) == '\0' && *(s + i) != *(accept + j))
 				return (matches);/*return if idx doesn't match*/
 		}
 		++;
 	}
 	return (matches); /* return num if all match till end */
-
 }
