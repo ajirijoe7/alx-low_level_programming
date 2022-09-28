c29c2122ebcd3f5c60de89405e222f8c4b0d5de6
Ajiroghene Jonathan O.Efe <ajirijoe@yahoo.com>
2022-09-28 15:23:13 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,10 +12,7 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 
-	if (x == 0)
-		return (0)
-
-	if (x == 1 || y == 0)
+	if (y == 0)
 		return (1);
 
 	return (x * _pow_recursion(x, y - 1));
