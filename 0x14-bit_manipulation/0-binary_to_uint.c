c7cae13711a67b8e2719102a062cd8be4180540f
Ajiroghene Jonathan O.Efe <ajirijoe@yahoo.com>
2022-10-27 18:48:08 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,39 @@
 #include "main.h"
 
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: b is pointing to a string of 0 and 1 chars
+ *
+ * Return: Converted number or 0 if
+ * there is one or more chars in the string b that is not 0 or 1
+ * or if b is NULL
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int res = 0, one, i;
+	int l = 0;
 
-	int len = 0, i;
-	unsigned int sum = 0;
+	if (b == 0)
+		return (0);
 
-	if (b == NULL)
-		return (sum);
 
-	/* find string length */
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
-
-	/* iterate string and if '1' then multiply by power of 2 */
-	/* get power of 2 via binary (e.g. 1<<2 = 100 in binary = 4) */
-	i = 0;
-	while (b[i])
+	while (b[l] != 0)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << len));
-		i++;
-		len--;
+		if (b[l] != '0' && b[l] != '1')
+			return (0);
+		l++;
 	}
 
-	return (sum);
-}
+	l = l - 1;
 
+	for (i = 0; l >= 0; i++, l--)
+	{
+		if (b[l] == '0')
+			one = 0;
+		else if (b[l] == '1')
+			one = 1;
+		one = one << i;
+		res = res | one;
+	}
+	return (res);
+}
