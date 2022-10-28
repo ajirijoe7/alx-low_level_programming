5774dea188920da59367938267fa55170491c50a
Ajiroghene Jonathan O.Efe <ajirijoe@yahoo.com>
2022-10-27 18:43:29 -0700

    101-password

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,12 +1,6 @@
-#ifndef HEADER_H
-#define HEADER_H
-
-#include <stdlib.h> /* NULL */
-
-/* helper function */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
-
-/* function prototypes for all files */
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -14,6 +8,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
 #endif
 
